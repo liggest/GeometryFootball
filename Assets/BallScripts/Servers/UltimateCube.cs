212c53ab46916eb683d8ec4c9aed07c7f90603d1
liggest <liggest@sina.cn>
2020-11-04 17:06:36 +0800

    小修改
    应用协程突破大招不能同时多发的限制
    优化了Bar和各个Player的物理

--- Assets/BallScripts/Servers/UltimateCube.cs
+++ Assets/BallScripts/Servers/UltimateCube.cs
@@ -8,9 +8,9 @@ namespace BallScripts.Servers
     public class UltimateCube : BaseUltimate
     {
         Player cubePlayer;
-        Vector3 playerPos;
-        private float timeUse = 0;
-        List<BaseStageObject> objList = new List<BaseStageObject>();
+        //Vector3 playerPos;
+        //private float timeUse = 0;
+        //List<BaseStageObject> objList = new List<BaseStageObject>();
         List<Vector3> initPosList = new List<Vector3> {
             new Vector3(1, 0, 1),
             new Vector3(-1, 0, 1),
@@ -41,6 +41,7 @@ namespace BallScripts.Servers
 
         public override void FixedUpdate()
         {
+            /*
             for (int i = 0; i < 8; i++)
             {
                 objList[i].transform.position = Vector3.MoveTowards(objList[i].transform.position, playerPos+endPosList[i], 10*Time.fixedDeltaTime);
@@ -57,7 +58,7 @@ namespace BallScripts.Servers
             {
                 Exit();
             }
-
+            */
             return;
         }
 
@@ -71,19 +72,23 @@ namespace BallScripts.Servers
         public override void Enter()
         {
             base.Enter();
-            playerPos = cubePlayer.transform.position;
-            Vector3 InitPos = playerPos + new Vector3(1, 0, 1);
+            //playerPos = cubePlayer.transform.position;
+            //Vector3 InitPos = playerPos + new Vector3(1, 0, 1);
             RigidBuildInfo info = new RigidBuildInfo
             {
                 category = StageObjectCategory.Dynamic,
                 id = StageManager.instance.GetMaxID(StageObjectCategory.Dynamic) + 1,
                 mass = 50,
+                useGravity = false,
                 angularDrag = 0.7f,
                 prefabName = "RoadBlock",
-                position = InitPos,
-                rotation = Quaternion.identity,
+                //position = InitPos,
+                //rotation = Quaternion.identity,
                 //initForce = Vector3.zero,
             };
+
+            Vector3 playerPos = cubePlayer.transform.position;
+            List<BaseStageObject> objList = new List<BaseStageObject>();
 
             for (int i = 0; i < 8; i++)
             {
@@ -94,12 +99,14 @@ namespace BallScripts.Servers
                 ServerSend.StageObjectSpawned(newInfo);
             }
             //Rigidbody rig = obj.GetComponent<Rigidbody>();
+            cubePlayer.StartCoroutine(BlockMove(playerPos, objList));
+            Exit();
         }
 
         public override void Exit()
         {
             base.Exit();
-
+            /*
             List <StageObjectPair> deSpawnObjList = new List<StageObjectPair>();
             for (int i = 0; i < 8; i++)
             {
@@ -109,11 +116,36 @@ namespace BallScripts.Servers
             ServerSend.StageObjectDespawned(deSpawnObjList);
             timeUse = 0;
             objList.Clear();
+            */
         }
-
+        /*
         private void TimeCount()
         {
             timeUse += Time.deltaTime;
+        }
+        */
+        IEnumerator BlockMove(Vector3 playerPos,List<BaseStageObject> objList)
+        {
+            float timeUse = 0;
+
+            while (timeUse < 4)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    objList[i].transform.position = Vector3.MoveTowards(objList[i].transform.position, playerPos + endPosList[i], 10 * Time.fixedDeltaTime);
+                    //Debug.Log("大招位置：" + objList[0].transform.position + "判定位置：" + playerPos + endPosList[0] + "距离：" + Vector3.Distance(objList[0].transform.position, playerPos + endPosList[0]));
+                }
+                timeUse += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            List<StageObjectPair> despawnObjList = new List<StageObjectPair>();
+            for (int i = 0; i < 8; i++)
+            {
+                GameManager.instance.DespawnStageObject(objList[i].category, objList[i].id);
+                despawnObjList.Add(new StageObjectPair { category = objList[i].category, id = objList[i].id });
+            }
+            ServerSend.StageObjectDespawned(despawnObjList);
+            Debug.Log($"大招结束，用时{timeUse}");
         }
     }
 }
